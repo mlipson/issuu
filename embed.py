553f33beed4e40e569c50b9a6c2912e14bea3770
Mike <mlipson@hotmail.com>
2019-09-24 22:55:49 -0700

    09.24.01

--- embed.py
+++ embed.py
@@ -1,5 +1,6 @@
 
 embeds = {
+'167': {'documentId': '190925053719-32ff8d3afa3a6dcb7b2bbafed7f34b24', 'title': 'C California Style & Culture', 'description': 'October 2019', 'dataconfigId': '6936490/67231417'},
 '166': {'documentId': '190814225652-bc30dc5e879180f7284fe63520d3d316', 'title': 'C California Style & Culture', 'description': 'September 2019', 'dataconfigId': '6936490/67231417'},
 '165': {'documentId': '190709153833-ecece7b999db98ef221c6c1b4a4d53e1', 'title': 'C Weddings', 'description': 'Fall 2019', 'dataconfigId': '6936490/67231417'},
 '164': {'documentId': '190610065444-cb312ebe3aef4dbbaa790b74e4b89233', 'title': 'C California Style & Culture', 'description': 'Summer 2019', 'dataconfigId': '6936490/70482254'},
